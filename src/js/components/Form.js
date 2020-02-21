1594c1b4fc65b29be2ff1eda8f28ef849905af64
Chris Turina <chris.turina@gmail.com>
2020-02-21 11:28:05 -0800

    create boilerplate html for the form

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -4,7 +4,22 @@ import React, { Component } from 'react';
 class Form extends React.Component {
   render() {
     return (
-      <div>Form </div>
+      <div>
+        <h1>Loan Application</h1>
+        <form>
+          <input placeholder="Car Price"/>
+          <br />
+          <input placeholder="Car Make" type="text" />
+          <br />
+          <input placeholder="Car Model" type="text" />
+          <br />
+          <input placeholder="Estimated Yearly income" />
+          <br />
+          <input placeholder="Estimated Credit Score" />
+          <br />
+          <button />
+        </form>
+      </div>
     );
   }
 }
