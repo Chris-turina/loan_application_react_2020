975f7582dcfc1fb1123fdbb65755d4b4961be457
Chris Turina <chris.turina@gmail.com>
2020-02-23 20:53:36 -0800

    add styling file to the styling folder

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import './App.css';
+import '../../styling/App.css';
 import Form from './Form';
 import Disqualify from './Disqualify';
 import NewAccount from './NewAccount';
--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Form.css';
+
 
 class Form extends React.Component{
   constructor(props) {
